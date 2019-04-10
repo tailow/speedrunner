56834da84049a417c46862065fb7d3f3508fb7bb
tailow-laptop <revitate@hotmail.com>
2019-04-10 08:41:24 +0300

    Added stopwatch UI

---
+++ Speedrunner/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using TMPro;
+
+public class GameManagement : MonoBehaviour
+{
+    Stopwatch stopwatch;
+
+    public TMP_Text stopwatchText;
+
+    void Start()
+    {
+        stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+    }
+
+    
+    void Update()
+    {
+        string timePassed = string.Format("{0:D1}:{1:D2}.{2:D3}", stopwatch.Elapsed.Minutes, stopwatch.Elapsed.Seconds, stopwatch.Elapsed.Milliseconds);
+
+        stopwatchText.text = timePassed;
+    }
+}
